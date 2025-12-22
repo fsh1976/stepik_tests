78271ed26628c17e444c154da9953bdd3610cda4
Farrukh <farrukhsharipov@hotmail.com>
2025-12-22 12:27:53 +0500

    Added LoginPage Class

---
+++ pages/login_page.py
@@ -0,0 +1,23 @@
+from .locators import LoginPageLocators
+from .base_page import BasePage
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        #assert True
+        text = self.browser.current_url
+        assert text.find("login")!=-1, "Login link is presented"
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.browser.find_element(*LoginPageLocators.LOGIN_FORM), "Login form presented"
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.browser.find_element(*LoginPageLocators.REGISTER_FORM), "Register form presented"
